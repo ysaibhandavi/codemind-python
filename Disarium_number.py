c6364488320f6acfce91e7de0c37e4e2b93125e2
ysaibhandavi <118281192+ysaibhandavi@users.noreply.github.com>
2023-10-05 14:36:48 +0530

    n=int(input()) n=str(n) l=len(n) n=int(n) m=n sum=0 while(n>0):     rem=n%10     sum=sum+int(rem**l)     n=n//10     l=l-1 if sum==m:     print("True") else:     print("False")

--- Disarium_number.py
+++ Disarium_number.py
@@ -4,12 +4,12 @@ l=len(n)
 n=int(n)
 m=n
 sum=0
-while n>0:
+while(n>0):
     rem=n%10
     sum=sum+int(rem**l)
     n=n//10
     l=l-1
-if m==sum:
+if sum==m:
     print("True")
 else:
     print("False")
