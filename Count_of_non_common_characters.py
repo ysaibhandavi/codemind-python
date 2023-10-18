05791bfc63ec1f0b933976f54c9a419353232cba
ysaibhandavi <118281192+ysaibhandavi@users.noreply.github.com>
2023-10-18 11:22:04 +0530

    a=input().lower().replace(" ","") b=input().lower().replace(" ","") c=[] for i in a:     if i not in b:         c.append(i) for i in b:     if i not in a:         c.append(i) p=set(c) print(len(p))

---
+++ Count_of_non_common_characters.py
@@ -0,0 +1,13 @@
+a=input().lower().replace(" ","")
+b=input().lower().replace(" ","")
+c=[]
+for i in a:
+    if i not in b:
+        c.append(i)
+for i in b:
+    if i not in a:
+        c.append(i)
+p=set(c)
+print(len(p))
+
+
